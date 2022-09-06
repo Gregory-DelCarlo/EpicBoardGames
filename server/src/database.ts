10728e75539d130decb2958072537a3871d6c483
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-06 13:04:49 -0700

    Created Users Schema and connected to our database

---
+++ server/src/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { User } from "./schemas/user";
+
+export async function connectToDatabase(uri: string) {
+    const db = await mongoose.createConnection(uri);
+
+    let userSchema = new mongoose.Schema(User);  
+    const userModel = db.model('UserModel', userSchema);
+    await userModel.create({
+        username: "First_user",
+        email: "Anemail@gmail.com",
+        password: "123456"
+    });
+    console.log(userModel.find({}));
+}
---
+++ server/src/schemas/user.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+export const User = new mongoose.Schema({
+    usernamme: {
+                    type: String,
+                    required: true,
+                    minLength: [3, 'Must be at least length 3, got {VALUE}'],
+                    unique: true
+                },
+    email: {
+                type: String,
+                required: true,
+                unique: true
+            },
+    password: {
+                type: String,
+                require: true,
+                minLength: [6, 'Must be at least length 6, got {VALUE}']
+            },
+    cart: Array<String>,
+    wishlist: Array<String>,
+    games: Array<String>,
+    friends: Array<String>
+})

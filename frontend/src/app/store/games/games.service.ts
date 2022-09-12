e2fa468462d231b623f63b0255f11d0af60e7070
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-12 12:02:08 -0700

    Began adding all games services

--- frontend/src/app/store/games/games.service.ts
+++ frontend/src/app/store/games/games.service.ts
@@ -8,17 +8,51 @@ import { Game } from './games.model';
 export class gameService {
   private url = "Http://localhost:3000";
   private games$: Subject<Game[]> = new Subject();
+  private game$: Subject<Game> = new Subject();
   constructor(private http: HttpClient) {}
 
   fetchGames(): Subject<Game[]> {
-    this.refreshGames();
+    this.allGames();
     return this.games$;
   }
-  private refreshGames() {
+
+
+// http calls
+  private allGames() {
     this.http.get<Game[]>(
       `${this.url}/games`
     ).subscribe(games => {
       this.games$.next(games);
     });
+  }
+
+  private getGame(game_id: string) {
+    this.http.get<Game>(
+      `${this.url}/games/${game_id}`
+    ).subscribe(game => {
+      this.game$.next(game);
+    })
+  }
+
+  private createGame(game: Game) {
+    this.http.post<Game>(
+      `${this.url}/games`, game
+    ).subscribe(game => {
+      this.game$.next(game);
+    });
+  }
+
+  private editGame(game_id: string, game: Game) {
+    this.http.post<Game>(
+      `${this.url}/games/${game_id}`, game
+    ).subscribe(game => {
+      this.game$.next(game);
+    });
+  }
+
+  private destroyGame(game_id: string) {
+    this.http.delete(
+      `${this.url}/games/${game_id}`
+    )
   }
 }

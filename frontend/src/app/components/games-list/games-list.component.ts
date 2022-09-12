8b7ccbf4ef1388dbaee7534ee71e0ebd52cec695
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-12 14:38:14 -0700

    Styled and updated games list component

--- frontend/src/app/components/games-list/games-list.component.ts
+++ frontend/src/app/components/games-list/games-list.component.ts
@@ -2,14 +2,15 @@ import { Component, Input, OnInit} from '@angular/core';
 import { Store } from '@ngrx/store';
 // import { FormsModule } from '@angular/forms';
 import { gameService } from 'src/app/store/games/games.service';
-import { retrieveGamesList } from 'src/app/store/games/games.actions';
+import { gameDispatchers } from 'src/app/store/games/games.actions';
 import { Game } from '../../store/games/games.model';
 import { Observable } from 'rxjs';
 import { selectAllGames } from './games-list.selector';
 
 @Component({
   selector: 'app-game-list',
-  templateUrl: './games-list.component.html'
+  templateUrl: './games-list.component.html',
+  styleUrls: ['./games-list.component.scss']
 })
 export class GameListComponent implements OnInit {
   games$: Observable<Game[]>;
@@ -19,13 +20,12 @@ export class GameListComponent implements OnInit {
     private store: Store // for accessing state
     ) {
       this.games$ = this.store.select(selectAllGames) //use selector
+      console.log(this.games$)
     }
 
     ngOnInit() {
-      // fetch games makes the AJAX call
-      this.gameService.fetchGames()   // send the games returned from api to the store
-        .subscribe((games) => {this.store.dispatch(retrieveGamesList({ games }))});
-
+      // calls api endpoint, and action to add games to state
+      gameDispatchers.retrieveGamesList(this.store, this.gameService)
   }
 }
 

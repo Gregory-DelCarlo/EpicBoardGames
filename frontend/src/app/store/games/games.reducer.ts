fe1699d6d501a750e4472a5f78a2a09784c9c9af
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-12 15:33:45 -0700

    Update gamesreducer

--- frontend/src/app/store/games/games.reducer.ts
+++ frontend/src/app/store/games/games.reducer.ts
@@ -8,5 +8,9 @@ export const gamesFeatureKey = "games"
 
 export const gamesReducer = createReducer(
     initialState,
-    on(gameActions.getGamesList, (state, { games }) => (games)) //get games from state then add to state
+    on(gameActions.getGamesList, (state, { games }) => (games)), //get games from state then add to state
+    on(gameActions.addGame, (state, { game }) => ({...state, game})),
+    on(gameActions.deleteGame, (state, { game }) => (
+      state.filter((gameEl) => { return gameEl._id != game._id })
+    ))
 );
